2e79b6de7df1d261a33029c1f9c08ec8a75b1e73
Mooner92 <chlaudgjs0989@gmail.com>
2022-02-21 15:04:20 +0900

    1

--- ex1.cpp
+++ ex1.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-#include <algorithm>
-#include "ex1.h"
+#include <string>
+using namespace std;
+int select;
 
+string statement("");
+string menu("");
 int main(){
+    cout << "**** 승리장에 오신 것을 환영합니다. *****\n";
+    cout << "짬뽕:1 짜장:2 군만두:3 종료:4";
+    cin >> select;
+    if(select == 1){
+        menu = "짬뽕";
+    }
+    else if (select == 2)
+    {
+       menu = "짜장";
+    }
+    else if (select == 3){
+        menu = "군만두";
+    }
+    else if (select == 4){
+        
+
+
+    }
     
+
+
 }
